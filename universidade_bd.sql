bb0474467bc5fa0e5ce910b8ab7cdcc65a377098
Soltore <106019301+Soltore@users.noreply.github.com>
2022-05-22 17:54:05 -0300

    Update universidade_bd.sql

--- universidade_bd.sql
+++ universidade_bd.sql
@@ -1,16 +1,16 @@
 --------------------------------------------------------------------------------------------------------------------------------------
->> Utilizando o banco de dados MySQL, crie um script contendo os comandos DDL necessários para criar um banco de dados e suas tabelas.
+-- Utilizando o banco de dados MySQL, crie um script contendo os comandos DDL necessários para criar um banco de dados e suas tabelas.
 --  O banco deverá ser chamado de “universidade-bd” e algumas das tabelas deste banco são as seguintes:
 --
->> ALUNO: armazena os dados pessoais dos alunos (nome, cpf, endereço...)
+-- ALUNO: armazena os dados pessoais dos alunos (nome, cpf, endereço...)
 --
->> PROFESSOR: armazena os dados dos professores da escola (nome, cpf, salário, carga horária...)
+-- PROFESSOR: armazena os dados dos professores da escola (nome, cpf, salário, carga horária...)
 --
->> CURSO: armazena os dados dos cursos ofertados pela universidade.
+-- CURSO: armazena os dados dos cursos ofertados pela universidade.
 --
->> MATRÍCULA: tabela que agrega as informações de alunos matriculados em determinados cursos.
+-- MATRÍCULA: tabela que agrega as informações de alunos matriculados em determinados cursos.
 --
->> a) Você deverá implementar obrigatoriamente estas três tabelas, porém ficará a seu critério a
+-- a) Você deverá implementar obrigatoriamente estas três tabelas, porém ficará a seu critério a
 -- definição dos atributos que cada tabela terá, e ainda poderá inserir mais tabelas desde que
 -- continue de acordo com a temática da tarefa. Não se esqueça de definir corretamente os
 -- relacionamentos entre as tabelas por meio da integridade referencial.
